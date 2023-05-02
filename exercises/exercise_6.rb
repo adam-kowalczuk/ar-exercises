fdca9f2946f5c9d23c4183b5fbce1ebb58ce3423
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-02 14:20:03 -0700

    Add employees to @store1 and @store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,7 +5,13 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
-# Your code goes here ...
+@store1.employees.create(first_name: 'Khurram', last_name: 'Virani', hourly_rate: 60)
+@store1.employees.create(first_name: 'John', last_name: 'Hopkins', hourly_rate: 60)
+@store1.employees.create(first_name: 'Alister', last_name: 'Crowley', hourly_rate: 60)
+
+@store2.employees.create(first_name: 'Jane', last_name: 'Fonda', hourly_rate: 60)
+@store2.employees.create(first_name: 'Betty', last_name: 'Davis', hourly_rate: 60)
+@store2.employees.create(first_name: 'Louise', last_name: 'Brooks', hourly_rate: 60)
