8005b4b8b02ae73d04c4059fabc48dd037a19d4a
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-02 13:57:56 -0700

    Get total and average annual_revenue, get count of stores with annual_revenue over M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,13 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
-# Your code goes here ...
+@total_revenue = Store.sum('annual_revenue')
+@average_revenue = Store.average('annual_revenue')
+@over_million = Store.where('annual_revenue >= ?', 1_000_000).count
+
+puts "Total annual revenue: #{@total_revenue}"
+puts "Average annual revenue: #{@average_revenue}"
+puts "Stores generating $1M or more in annual sales: #{@over_million}"
