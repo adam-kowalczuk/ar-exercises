accd95d714a202211e6419183ecc9ec42f0fd737
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-02 14:59:55 -0700

    Add code to prompt user for store name and return errors

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,14 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
-# Your code goes here ...
+print 'Please enter a store name:'
+
+answer = $stdin.gets.chomp
+
+new_store = Store.create(name: "#{answer}")
+new_store.save
+
+puts new_store.errors.full_messages
